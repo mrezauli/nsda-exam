a06bc7583ca7f9bc12fcdd0feeee502f4ad68a6d
Rezaul Islam <rezaul.cse.mbstu@gmail.com>
2022-07-07 14:41:33 +0600

    completed musical band

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@ import * as bootstrap from 'bootstrap';
 import 'bootstrap'
 import '../scss/custom.scss'
 
+function showDatetime() {
+    let today = new Date();
+    let date = today.toDateString();
+    let time = today.toTimeString();
+    let dateTime = date + ' ' + time;
+    document.getElementById("clock").innerHTML = dateTime;
+    setTimeout(showDatetime, 1000);
+}
+
+showDatetime();
+
 console.log('Hello world!');
